8f32f3685879ad6ec5241c92dfc7e78e767c4951
AdhamAbdo <adhamabdo898@gmail.com>
2026-03-31 04:56:23 +0200

    fix: remove next-pwa, add turbopack config for Next.js 16

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  turbopack: {},
 }
 
-const withPWA = require('@ducanh2912/next-pwa').default({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-  cacheOnFrontEndNav: true,
-  reloadOnOnline: true,
-})
-
-module.exports = withPWA(nextConfig)
+module.exports = nextConfig
